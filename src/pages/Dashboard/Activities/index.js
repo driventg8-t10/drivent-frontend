8309c8ae9b2b4c8f8c0775e2bdb0d03245d83a77
Pericles <periclesebmendes@gmail.com>
2023-08-13 17:59:15 -0300

    feat: error messages

--- src/pages/Dashboard/Activities/index.js
+++ src/pages/Dashboard/Activities/index.js
@@ -6,15 +6,19 @@ import { useEffect } from 'react';
 import { getEventInfo } from '../../../services/eventApi';
 import { useState } from 'react';
 import useToken from '../../../hooks/useToken';
+import { getBookingByUserId } from '../../../services/bookingApi';
+import { getUserTicket } from '../../../services/ticketApi';
 
 export default function Activities() {
   const [datesArray, setDate] = useState([]);
   const [selectedDate, setSelected] = useState([]);
   const [activities, setActivities] = useState([]);
   const [subscribedReloadFlag, setFlag] = useState(0);
+  const [content, setContent] = useState('');
 
   const token = useToken();
-  useEffect(() => { 
+  useEffect(() => {
+    validateTicketAndHotel();
     const info = getEventInfo();
     info
       .then((res) => {
@@ -23,6 +27,41 @@ export default function Activities() {
       .catch((err) => {
       });
   }, []);
+
+  function validateTicketAndHotel() {
+    const ticket = getUserTicket(token);
+    ticket
+      .then((res) => {
+        console.log(res);
+        if(res.status === 'RESERVED') {
+          setContent('No Payment');
+          return 1;
+        }
+
+        if(res.TicketType.includesHotel === true && res.TicketType.isRemote === false) {
+          const booking = getBookingByUserId(token);
+          booking
+            .then((res) => {
+            })
+            .catch((err) => {
+              if(err.response.status === 404) {
+                setContent('No Hotel');
+                return 1;
+              };
+            });
+          return 1;
+        }
+        else if(res.TicketType.includesHotel === false || res.TicketType.isRemote === true) {
+          setContent('No Host');
+        }
+      })
+      .catch((err) => {
+        if(err.response.status === 404) {
+          setContent('No Ticket');
+          return 1;
+        };
+      });
+  }
 
   function dateFormat(startDate, endDate) {
     const datesInRange = [];
@@ -48,9 +87,54 @@ export default function Activities() {
     return `${year}-${month}-${day}`;
   };
 
+  if(content === 'No Host') {
+    return(
+      <> 
+        <StyledTypography variant="h4">Escolha de atividades</StyledTypography>
+        <TextContainer>
+          <h1>Sua modalidade de ingresso não necessita escolher atividade.</h1>
+          <h1>Você terá acesso a todas as atividades.</h1>
+        </TextContainer>
+      </>
+    );
+  }
+
+  if(content === 'No Hotel') {
+    return(
+      <> 
+        <StyledTypography variant="h4">Escolha de atividades</StyledTypography>
+        <TextContainer>
+          <h1>Você precisa escolher o hotel antes de fazer a escolha de atividades.</h1>
+        </TextContainer>
+      </>
+    );
+  }
+
+  if(content === 'No Payment') {
+    return(
+      <> 
+        <StyledTypography variant="h4">Escolha de atividades</StyledTypography>
+        <TextContainer>
+          <h1>Você precisa ter confirmado pagamento antes de fazer a escolha de atividades.</h1>
+        </TextContainer>
+      </>
+    );
+  }
+
+  if(content === 'No Ticket') {
+    return(
+      <> 
+        <StyledTypography variant="h4">Escolha de atividades</StyledTypography>
+        <TextContainer>
+          <h1>Você precisa escolher um ticket antes de fazer a escolha de atividades!</h1>
+        </TextContainer>
+      </>
+    );
+  }
+
   return (
     <>
-      <StyledTypography variant="h4">Escolha as atividades</StyledTypography>
+      <StyledTypography variant="h4">Escolha de atividades</StyledTypography>
       <DateSelection 
         datesArray = {datesArray}
         setSelected = {setSelected}
@@ -75,4 +159,20 @@ export default function Activities() {
 
 const StyledTypography = styled(Typography)`
   margin-bottom: 30px!important;
+`;
+
+const TextContainer = styled.div`
+  width: 100%;
+  height: 80%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  h1 {
+    color: #8E8E8E!important;
+    font-family: Roboto;
+    font-size: 20px;
+    font-weight: 400;
+    line-height: 23px;
+  }
 `;
